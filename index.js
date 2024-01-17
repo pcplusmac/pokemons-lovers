5c4925782fe8db37fb84910045dbf8dfdc22a54a
el-sam <samurai.h@yahoo.com>
2024-01-17 15:03:09 +0800

    format the way for displaying the details on the page

--- index.js
+++ index.js
@@ -7,7 +7,7 @@ function init (){
 async function fetchPokemons (){
     const res = await fetch("https://pokeapi.co/api/v2/pokemon")
     const data = await res.json()
-    // console.log("data",data) 
+    console.log("data",data) 
     data.results.forEach(item => fetchPokeDetails(item.url))
 }
 
@@ -22,11 +22,31 @@ function fetchPokeDetails(url){
 }
 
 function renderPokemon(pokemon){
-    let ulpoke = document.querySelector('#detail-list')
-    console.log("ulpoke:",ulpoke)
+    let pokeList = document.querySelector('#cards-container')
+    console.log("ulpoke:",pokeList)
+    let ulpoke = document.createElement('ul')
+    ulpoke.id = "poke-card"
     let lipoke = document.createElement('li')
-    lipoke.innerText = pokemon.name
+    lipoke.innerHTML = `
+        <img src="${pokemon.sprites.back_default}" alt="image coming">
+        <div class="detail-list">
+            <h4>Nanme: ${pokemon.name}</h4>
+            <ul class="details">
+                <li>weight: ${pokemon.weight}.</li>
+                <li>Height: ${pokemon.height}</li>
+                <li class="long-details">Moves:
+                    <ol>
+                        ${ pokemon.moves.map((poke) =>`<li> ${poke.move.name} </li>`  ) }
+                    </ol>
+                </li>
+                
+                <li></li>
+            </ul>
+        </div>
+    `
+    
     ulpoke.appendChild(lipoke)
+    pokeList.appendChild(ulpoke)
 
 }
 
